975975bcf12e7f5c6de3eb2823a57589fd429a2d
TanyaFreebie <fjodrova.tanya@gmail.com>
2021-06-18 21:27:44 +0300

    ClassTask

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Number Declaration
+        int firstNumber;
+        int secondNumber;
+        int answer;
+        //NUmber Initialized
+        firstNumber = 5;
+        secondNumber = 7;
+        answer = 0;
+        //Math Operator
+        String operator ="-";
+
+        if(operator.equals("+")){
+            answer = firstNumber + secondNumber;
+        } else if(operator.equals("-")){
+            answer = firstNumber - secondNumber;
+        }//assigment for x and /
+
+        System.out.println(answer);
+    }
+}
