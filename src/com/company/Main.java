70be5cb493f448d7d2eb11bd45197b268e7e780a
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-06-21 20:27:12 +0300

    Second lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //Number Declaration
-        int firstNumber;
-        int secondNumber;
-        int answer;
+/*        //Number Declaration
+        double firstNumber;
+        double secondNumber;
+        double answer;
 
         //NUmber Initialized
-        firstNumber = 5;
+        firstNumber = 22;
         secondNumber = 7;
         answer = 0;
 
         //Math Operator
-        String operator ="+";
+        String operator ="/";
 
         if(operator.equals("+")){
             answer = firstNumber + secondNumber;
@@ -25,7 +25,53 @@ public class Main {
         } else if(operator.equals("/")) {
             answer = firstNumber / secondNumber;
         }
+ //       System.out.println((float)answer);
 
-        System.out.println(answer);
+        int five =5;
+        //        Post-increment means you  increasing it by one AFTER
+        System.out.println("Post-increment value:" + five++);
+//         five+= 1; the same as five = five + 1;
+//        Pre-increment  means as the same before
+        System.out.println("Pre-increment value:" + ++five);// This works only on icrement be one
+
+ */
+  /*      boolean sixLessThan =6 < 8;
+        boolean fiveGreaterThan = 5 > 9;
+        // && - AND only works if both statements are true
+        // || OR work if either is true, give false only is both are false
+        // !- NOT
+        // != NOT EQUAL
+        // == EQUAL this simply checks for equality
+       if(sixLessThan == fiveGreaterThan){
+            System.out.println("OR works");
+        } else {
+            System.out.println("OR was skipped");
+        }
+*/
+        //LOOPS
+ /*       for(int i = 1; i <=10; i++){
+            //check for even numbers
+            //an even number is number divisible by 1 without remainders
+//            System.out.println(i + "modulo 2 = " + i % 2);
+            if(i % 2 == 0){
+                System.out.println("Current even number:" + i);
+            }else{
+                System.out.println("Current odd number" + i);
+            }
+        }
+*/
+ /*       int counter = 1;
+        while(counter <= 10){
+//            System.out.println(counter);
+            counter++;
+        }
+
+        do{
+ //           System.out.println(counter);
+            counter++;
+        } while(counter <= 10);
+ */
+
+
     }
 }
